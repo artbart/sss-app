faeba6e088b84e424d9acb3a59c24b27216acc56
Arturas Bobinas <abobinas@gmail.com>
2026-06-26 19:48:33 +0000

    Chat-hero card on stories.html opens in same tab (not _blank)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //
 // Cache versioning: bump CACHE_VERSION when shipping a breaking change.
 
-const CACHE_VERSION = "sss-app-v10";
+const CACHE_VERSION = "sss-app-v11";
 const SHELL = [
   "/",
   "/index.html",
