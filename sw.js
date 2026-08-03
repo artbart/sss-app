f713235d640242cf53523961aca895e89f7b53f9
Arturas via Claude <abobinas@gmail.com>
2026-08-03 09:40:20 +0300

    Home: exclude library books from other-stories rail + make View-all link expand inline (fixes leak + dead #more-stories anchor)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //
 // Cache versioning: bump CACHE_VERSION when shipping a breaking change.
 
-const CACHE_VERSION = "sss-app-v28";
+const CACHE_VERSION = "sss-app-v29";
 const SHELL = [
   "/",
   "/index.html",
